4c536229a742c1d93558b3c273fd065da147fabc
Børge Bjørnli Nygård <borge.b.nygard@gmail.com>
2014-02-15 18:12:28 +0100

    Added README.

---
+++ README.ml
@@ -0,0 +1,14 @@
+**adapt** is command line utility written in C++ for fitting shapes to
+points.
+
+# Build prerequisites #
+
+* linux
+* gcc 4.8
+* SCons
+
+# Getting started #
+
+1. $ git clone https://github.com/borgebjo/adapt.git
+2. $ cd adapt
+3. $ adapt --help
