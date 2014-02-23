6a1331a46d24e451b9636a8d4859e3f6af036e7b
Børge Bjørnli Nygård <borge.b.nygard@gmail.com>
2014-02-23 22:55:23 +0100

    Added covariance function.

--- src/math/statistics.h
+++ src/math/statistics.h
@@ -29,5 +29,27 @@ double Variance(const T& container)
    return squared_sum/(static_cast<double>(container.size()) - 1);
 }
 
+
+/// Computes the sample covariance between the stocastic x variables
+/// and the stocastic y variables given in the \c x_values and \c
+/// y_values containter respectively.
+template <typename T, typename U>
+double Covariance(const T& x_values, const U& y_values)
+{
+   if (x_values.size() != y_values.size())
+      throw std::invalid_argument("x_values and y_values passed to Covariance has different sizes.");
+   if (x_values.size() < 2)
+      throw std::invalid_argument("Arguments with size less than 2 passed to Covariance.");
+
+   auto x_mean = Mean(x_values);
+   auto y_mean = Mean(y_values);
+   auto inner_product = double(0.0);
+   for (unsigned int i = 0; i < x_values.size(); ++i)
+   {
+      inner_product += (x_values[i]-x_mean)*(y_values[i]-y_mean);
+   }
+   return inner_product/(static_cast<double>(x_values.size())-1);
+}
+
 } // namespace math
 } // namespace adapt
--- src/math/test/statisticstest.cpp
+++ src/math/test/statisticstest.cpp
@@ -53,6 +53,40 @@ BOOST_AUTO_TEST_CASE(test_variance_throws_invalid_argument)
    BOOST_CHECK_THROW(Variance(one_value), std::invalid_argument);
 }
 
+// ---
+BOOST_AUTO_TEST_CASE(test_covariance_throws_if_different_sizes)
+{
+   std::vector<int> xvalues = {1,1,1};
+   std::vector<int> yvalues = {1,1};
+
+   BOOST_CHECK_THROW(Covariance(xvalues,yvalues), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(test_covariance_int_trivial)
+{
+   std::vector<int> xvalues = {1,1};
+   std::vector<int> yvalues = {1,1};
+   double expected = 0.0;
+   BOOST_CHECK_CLOSE(expected, Covariance(xvalues, yvalues), 0.01);
+}
+
+BOOST_AUTO_TEST_CASE(test_covariance_int)
+{
+   std::vector<int> xvalues = {1,2};
+   std::vector<int> yvalues = {2,4};
+   double expected = 1;
+   BOOST_CHECK_CLOSE(expected, Covariance(xvalues, yvalues), 0.01);
+}
+
+BOOST_AUTO_TEST_CASE(test_covariance_throws_invalid_argument)
+{
+   std::vector<double> no_values;
+   BOOST_CHECK_THROW(Covariance(no_values,no_values), std::invalid_argument);
+
+   std::vector<double> one_value = {1.0};
+   BOOST_CHECK_THROW(Covariance(one_value,one_value), std::invalid_argument);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 } // namespace test
