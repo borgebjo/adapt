8ac2442082c62520912abe5f0b078d42552c3e16
Børge Bjørnli Nygård <borge.b.nygard@gmail.com>
2014-02-23 16:59:25 +0100

    Read points from stdin.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "options.h"
 #include "fitter.h"
+#include "parser2d.h"
+#include "parsererror.h"
+#include "math/point.h"
+
 #include <memory>
 #include <iostream>
 
@@ -8,5 +12,22 @@ int main(int argc, char *argv[])
    adapt::Options options;
 
    std::unique_ptr<adapt::Fitter> fitter = options.parse(argc, argv);
+   adapt::Parser2d parser(std::cin);
+   try
+   {
+      auto points = parser.parse();
+
+      std::cout << "Read the following points from stdin" << std::endl;
+      for (auto point: points)
+      {
+	 std::cout << point << std::endl;
+      }
+   }
+   catch (adapt::ParserError& e)
+   {
+      std::cout << "Error occured." << std::endl;
+      std::cout << e.what() << std::endl;
+      return 1;
+   }
    return 0;
 }
