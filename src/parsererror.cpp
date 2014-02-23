a576b134abf1fd869423f6d030c31ed196d4031d
Børge Bjørnli Nygård <borge.b.nygard@gmail.com>
2014-02-23 13:57:44 +0100

    Implemented ParserError class.

--- src/parsererror.cpp
+++ src/parsererror.cpp
@@ -1,14 +1,19 @@
 #include "parsererror.h"
+#include <sstream>
 
 namespace adapt {
 
 ParserError::ParserError(int number) :
    theParserNumber{number}
 {
+   std::stringstream ss;
+   ss << "Parsing error at item number " << number;
+   theMessage = ss.str();
 }
 
 const char* ParserError::what() const noexcept(true)
 {
+   return theMessage.c_str();
 }
 
 } // namespace adapt
